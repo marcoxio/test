0088788b4431e97115ed758abf6c701e93eb85ab
marcoxio <manjiso2@hotmail.com>
2016-11-30 18:26:47 -0500

    "Codigo Smile"

--- src/prueba_jenkins/Prueba_Jenkins.java
+++ src/prueba_jenkins/Prueba_Jenkins.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prueba_jenkins;
 import java.util.Scanner;
 /**
@@ -11,15 +6,11 @@ import java.util.Scanner;
  */
 public class Prueba_Jenkins {
 
-   private Prueba_Jenkins(){
-   
-   }
-    
     
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        System.out.println("Holaaa Mundo");     
+        
+        System.err.println("Holaaa Mundo");     
     }
     
 }
