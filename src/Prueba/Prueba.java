672d63811b6264b5b55da3720a16a0e36530f699
marcoxio <manjiso2@hotmail.com>
2016-11-30 18:29:40 -0500

    "Se cambio la clase principal"

---
+++ src/Prueba/Prueba.java
@@ -0,0 +1,16 @@
+package Prueba;
+
+/**
+ *
+ * @author Marco
+ */
+public class Prueba {
+
+    
+    public static void main(String[] args) {
+        
+        
+        System.err.println("Holaaa Mundo");     
+    }
+    
+}
